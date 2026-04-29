8a911b4727ed2b259e855ae4431b54bf5f87087b
Alex <dariangosztafio@gmail.com>
2026-04-29 12:00:00 +0800

    util: add restrict qualifiers and pure alias to blake2b API

--- include/util.h
+++ include/util.h
@@ -1,24 +1,22 @@
 #pragma once
-
 #define AXIOM_BLAKE2B_OUT_LEN 32
 #define AXIOM_BLAKE2B_BLOCK_LEN 128
-
 typedef struct {
     uint64_t h[8];
     uint64_t t;
     uint8_t  buf[AXIOM_BLAKE2B_BLOCK_LEN];
     size_t   buflen;
 } axiom_blake2b_ctx;
-
-void axiom_blake2b_init(axiom_blake2b_ctx *ctx);
-void axiom_blake2b_update(axiom_blake2b_ctx *ctx,
-    const void *data, size_t len);
-void axiom_blake2b_final(axiom_blake2b_ctx *ctx,
+void axiom_blake2b_init(axiom_blake2b_ctx *restrict ctx);
+void axiom_blake2b_update(axiom_blake2b_ctx *restrict ctx,
+    const void *restrict data, size_t len);
+void axiom_blake2b_final(axiom_blake2b_ctx *restrict ctx,
     uint8_t out[AXIOM_BLAKE2B_OUT_LEN]);
-
-int axiom_blake2b_file(const char *path,
+int axiom_blake2b_file(const char *restrict path,
     uint8_t out[AXIOM_BLAKE2B_OUT_LEN]);
-
-void axiom_hex_encode(const uint8_t *in, size_t inlen, char *out);
-int axiom_hex_decode(const char *in, uint8_t *out, size_t outlen);
+void axiom_hex_encode(const uint8_t *restrict in, size_t inlen,
+    char *restrict out);
+int axiom_hex_decode(const char *restrict in, uint8_t *restrict out,
+    size_t outlen);
 long long axiom_epoch_ns(void);
+long long axiom_epoch_ns_pure(void) __attribute__((pure));
