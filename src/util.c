9492d5bfbee4efb0c7a98e92b4143ce43c136968
Alex <dariangosztafio@gmail.com>
2026-04-29 14:00:00 +0800

    util: add RFC 7693 section references to blake2b

--- src/util.c
+++ src/util.c
@@ -1,4 +1,5 @@
 #include "z_jail.h"
+// RFC 7693 Section 2.3 - Initialization Vector
 #include <time.h>
 
 static const uint64_t IV[8] = {
@@ -24,6 +25,7 @@ static const uint8_t SIGMA[12][16] = {
 };
 
 #define ROTR64(x, n) (((x) >> (n)) | ((x) << (64 - (n))))
+// G mixing function: a += b + x; d = rot(d ^ a, 32); ... (RFC 7693 Section 2.4)
 #define G(v, a, b, c, d, x, y) \
     do { \
         v[a] = v[a] + v[b] + x; \
